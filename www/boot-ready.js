cf0286200e20f8da53013ce6ce3f0b1bc41724fd
Boris <konovalova-2017@mail.ru>
2026-07-17 21:33:33 +0300

    Use single guarded Media3 bridge

--- www/boot-ready.js
+++ www/boot-ready.js
@@ -2,7 +2,7 @@
 'use strict';
 if(document?.querySelector&&document?.createElement&&!document.querySelector('script[data-n54-media3]')){
  const script=document.createElement('script');
- script.src='media3-bridge-v2.js';
+ script.src='media3-bridge.js';
  script.dataset.n54Media3='1';
  (document.head||document.body)?.append?.(script);
 }
